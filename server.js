7421d50ad8c397e2f94beec418fbebffd9759c8e
Damanjot Singh <damanjotsingh1712@gmail.com>
2022-12-08 15:14:21 -0500

    update

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB 322- TEST 6
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: __Damanjot Singh____ Student ID: __148285216_____ Date: ____08/12/2022_____
+*
+*  Online(Cyclic) URL:  https://lively-frog-buckle.cyclic.app 
+********************************************************************************/
+
 var express = require("express");
 var path = require("path");
 const app = express();
